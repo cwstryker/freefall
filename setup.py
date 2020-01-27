5ce947bb746b0f4c59f0ffa6df37e0fd0b8cbc73
Chad Stryker <chadwick@stryker5.org>
2020-01-26 21:16:52 -0800

    Added a setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='freefall',
+    version='0.1.1',
+    packages=['freefall'],
+    url='',
+    license='MIT',
+    author='Chad Stryker',
+    author_email='',
+    description='A Python package module for simulating falling objects with aerodynamic drag.'
+)
