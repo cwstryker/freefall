3a16ccc2039f736a1f3df16754da3897dee404fe
Chad Stryker <chadwick@stryker5.org>
2020-01-27 21:13:01 -0800

    Added a setup.py file.

--- freefall/__init__.py
+++ freefall/__init__.py
@@ -1,2 +1,3 @@
 from .simulator import FallingObject
 from .simulator import simulate
+from .simulator import find_vx_vy
--- freefall/simulator.py
+++ freefall/simulator.py
@@ -1,8 +1,15 @@
 from collections import namedtuple
+import math
 
 FallingObject = namedtuple("FallingObject", ["mass", "drag"])
 SimResult = namedtuple("SimResult", ["x", "y", "vx", "vy", "ax", "ay", "t"])
 
+
+def find_vx_vy(*, speed, angle):
+    rads = math.radians(angle)
+    vx = speed * math.acos(rads)
+    vy = speed * math.asin(rads)
+    return vx, vy
 
 def simulate(
     target,
@@ -37,8 +44,19 @@ def simulate(
         x.append(x[-1] + epsilon * vx[-1])
         y.append(y[-1] + epsilon * vy[-1])
 
-        ax.append(-vx[-1] ** 2 * target.drag / target.mass)
-        ay.append((vy[-1] ** 2 * target.drag / target.mass) - gravity)
+        if vx[-1] >= 0:
+            direction = -1
+        else:
+            direction = 1
+        a = direction * vx[-1] ** 2 * target.drag / target.mass
+        ax.append(a)
+
+        if vy[-1] >= 0:
+            direction = -1
+        else:
+            direction = 1
+        a = direction * vy[-1] ** 2 * target.drag / target.mass - gravity
+        ay.append(a)
 
         vx.append(vx[-1] + epsilon * ax[-1])
         vy.append(vy[-1] + epsilon * ay[-1])
