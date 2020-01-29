a209b1cfe9862d74ca0a587a35a95077dc9b2709
Stryker, Chad <Chad.Stryker@tektronix.com>
2020-01-29 15:44:55 -0800

    Improvements to the FRC power cell shooter example

--- examples/frc_powercell_shooter.py
+++ examples/frc_powercell_shooter.py
@@ -1,28 +1,40 @@
 from freefall.falling_objects import frc_power_cell
 from freefall.simulators import simulate_earth_surface
+from freefall.simulators import terminate_vy_less_zero
 from freefall.utilities import find_vx_vy, float_range
 
 import matplotlib.pyplot as plt
 
 X_INITIAL = 0       # m
 Y_INITIAL = 27/40   # m
-SPEED = 10          # m/s
+SPEED = 5           # m/s
 ANGLE = 50          # degrees
 
-fig, ax = plt.subplots()
+fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2)
 
-for i in float_range(2, 7, 0.1):
+# Plot the trajectory over several valves of speed
+ax0.set(title="Power Cell Trajectory by Speed", xlabel="Distance (m)", ylabel="Height (m)")
+ax0.grid()
+for i in float_range(2, 8, 0.1):
+    
+    # run the simulation
     vx_initial, vy_initial = find_vx_vy(speed=i, angle=ANGLE)
-    results = simulate_earth_surface(frc_power_cell, X_INITIAL, Y_INITIAL, vx_initial, vy_initial)
+    results = simulate_earth_surface(frc_power_cell, X_INITIAL, Y_INITIAL, vx_initial, vy_initial, terminator=terminate_vy_less_zero)
 
     # Plot the results
-    ax.plot(results.x, results.y)
+    ax0.plot(results.x, results.y)
 
-# Format and annotate the graph
-ax.grid()
-plt.xlabel("Distance (m)")
-plt.ylabel("Height (m)")
-plt.title("Simulation of Power Cell Free Fall")
+# Plot the trajectory over several valves of angle
+ax1.set(title="Power Cell Trajectory by Angle", xlabel="Distance (m)")
+ax1.grid()
+for i in float_range(10, 90, 2):
+    
+    # run the simulation
+    vx_initial, vy_initial = find_vx_vy(speed=SPEED, angle=i)
+    results = simulate_earth_surface(frc_power_cell, X_INITIAL, Y_INITIAL, vx_initial, vy_initial, terminator=terminate_vy_less_zero)
+
+    # Plot the results
+    ax1.plot(results.x, results.y)
 
 # Display the graph
 plt.show()
