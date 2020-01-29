e16b7a332085e6587801d1a68d79d89d596b2864
Stryker, Chad <Chad.Stryker@tektronix.com>
2020-01-29 13:31:35 -0800

    Improvements to the setup.py file

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import re
+
+version = re.search(
+    r'^__VERSION__\s*=\s*"(.*)"', open("freefall/__init__.py").read(), re.M
+).group(1)
 
 setup(
-    name='freefall',
-    version='0.1.1',
-    packages=['freefall'],
-    url='',
-    license='MIT',
-    author='Chad Stryker',
-    author_email='',
-    description='A Python package module for simulating falling objects with aerodynamic drag.'
+    name="freefall",
+    version=version,
+    packages=find_packages(),
+    url="",
+    license="MIT",
+    author="Chad Stryker",
+    author_email="",
+    description="A Python package module for simulating falling objects with aerodynamic drag.",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Education",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
 )
