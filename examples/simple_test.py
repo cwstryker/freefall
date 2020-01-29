604921526e5733b7c041ad7e4458f9a178c7792b
Stryker, Chad <Chad.Stryker@tektronix.com>
2020-01-29 13:30:07 -0800

    Modified example to use floating point range generator.

--- examples/simple_test.py
+++ examples/simple_test.py
@@ -11,9 +11,34 @@ Y_INITIAL = 27/40   # m
 SPEED = 10          # m/s
 ANGLE = 50          # degrees
 
+"""
+Desc : This function generates a float range of numbers w/o using any library.
+
+Params :
+A (int/float) : First number in the range
+L (int/float) : Last number in the range
+D (int/float) : Step or the common difference
+"""
+def float_range(A, L=None, D=None):
+    #Use float number in range() function
+    # if L and D argument is null set A=0.0 and D = 1.0
+    if L == None:
+        L = A + 0.0
+        A = 0.0
+    if D == None:
+        D = 1.0
+    while True:
+        if D > 0 and A >= L:
+            break
+        elif D < 0 and A <= L:
+            break
+        yield float(("%g" % A)) # return float number
+        A = A + D
+#end of function float_range()
+
 fig, ax = plt.subplots()
 
-for i in range(1, 10, 1):
+for i in float_range(2, 7, 0.1):
     vx_initial, vy_initial = find_vx_vy(speed=i, angle=ANGLE)
     power_cell = FallingObject(mass=MASS, drag=DRAG_COEF)
     results = simulate_earth_surface(power_cell, X_INITIAL, Y_INITIAL, vx_initial, vy_initial)
